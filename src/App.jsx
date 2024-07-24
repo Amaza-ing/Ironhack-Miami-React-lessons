aecfa737f3492e7cd40445c36fd102a6cdca02ff
Adrián Maza <adri_mv1@hotmail.com>
2024-07-24 16:31:09 +0200

    react basics

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import HeaderComponent from "./components/HeaderComponent";
 import MovieList from "./components/MovieList";
 
 function App() {
-  let title = "This is my React Project!";
+  let title = "This is my React Project!!";
 
   return (
     <>
