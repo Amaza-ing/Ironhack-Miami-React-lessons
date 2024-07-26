ff8b408a26634dab8229054d62c1c50d3947cfdb
Adrián Maza <adri_mv1@hotmail.com>
2024-07-26 16:44:04 +0200

    lifecycle, useEffect() & router

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
+  // <React.StrictMode> -> commented so we don't execute some things twice
   <BrowserRouter>
     <App />
   </BrowserRouter>
